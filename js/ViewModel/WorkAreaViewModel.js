f1437b0b892731ffcb1eb572ae9867f0d222c79c
Michael <mkorolev@sfdev.com>
2014-06-24 10:05:24 +0400

    test fix IE

--- js/ViewModel/WorkAreaViewModel.js
+++ js/ViewModel/WorkAreaViewModel.js
@@ -546,7 +546,7 @@ var WorkAreaViewModel = function (app) {
 
             c.width = svgWidth;
             c.height = svgHeight;
-            ifrmWin.$('body').append(c);
+            ifrmWin.getElementsByTagName('body')[0].innerHTML = c;
             if (typeof FlashCanvas != "undefined") {
                 FlashCanvas.initElement(c);
             }
