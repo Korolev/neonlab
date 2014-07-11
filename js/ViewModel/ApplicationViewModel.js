2c3df5e452e667c31ef2b7d816bcfe40b6f7a683
Michael <mkorolev@sfdev.com>
2014-07-11 15:41:50 +0400

    add another filter by deepII

--- js/ViewModel/ApplicationViewModel.js
+++ js/ViewModel/ApplicationViewModel.js
@@ -466,7 +466,6 @@ console.log(val && self.WorkArea.showOptionsDialog());
         var testUsedDiodType = function(diodType){
           var allowed = diodeSearch(v);
             return allowed.indexOf(diodType) == -1;
-            
         };
         if (selectedType !== undefined && self.usedDiodType() === defDiod || selectedType !== undefined && testUsedDiodType(self.usedDiodType())) {
             self.usedDiodTypes([]);
